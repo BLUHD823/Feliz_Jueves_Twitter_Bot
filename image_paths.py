d04b50a1391a3891d91cc98baf2e2922ce80a9d7
unknown <alejodeje@gmail.com>
2024-01-02 15:39:01 -0300

    cambio del if en image_paths

--- image_paths.py
+++ image_paths.py
@@ -9,13 +9,7 @@ def open_json(json_path):
 def get_path(datos,number):
     for paths in datos:
         for name,path in paths.items():
-            #print(f"{name}:{path}")
-            if number in[1,2,3,4] :
-                title = "image_"+ str(number)
-                if name == title:
-                    image_path = path
-                    return image_path
-            elif number in[5,6,7]:
+            if 1 <= number <= 7:
                 title = "image_" + str(number)
                 if name == title:
                     image_path = path
